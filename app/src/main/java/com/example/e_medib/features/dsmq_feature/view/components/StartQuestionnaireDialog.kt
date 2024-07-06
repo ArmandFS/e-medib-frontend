85dd82bb0becb0d6f45bf05f3350b4fd5b3fd9b6
ArmandFS <actuallyarmand@gmail.com>
2024-07-07 00:27:18 +0700

    centered and fixed dialog start questionnaire buttons

--- app/src/main/java/com/example/e_medib/features/dsmq_feature/view/components/StartQuestionnaireDialog.kt
+++ app/src/main/java/com/example/e_medib/features/dsmq_feature/view/components/StartQuestionnaireDialog.kt
@@ -1,7 +1,12 @@
 package com.example.e_medib.features.dsmq_feature.view.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -11,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun StartQuestionnaireDialog(
@@ -39,25 +45,30 @@ fun StartQuestionnaireDialog(
                     modifier = Modifier.fillMaxWidth()
                     )
                    },
-            confirmButton = {
-                Column(horizontalAlignment = Alignment.CenterHorizontally){
-                    Button(
-                        onClick = { onConfirm() },
-                        colors = ButtonDefaults.buttonColors(containerColor = confirmButtonColor),
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+            buttons = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)        ,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
                     ) {
-                        Text(text = "Iya", color = Color.White)
-                    }
-                }
-            },
-            dismissButton = {
-                Column(horizontalAlignment = Alignment.CenterHorizontally){
-                    Button(
-                        onClick = { onDismiss() },
-                        colors = ButtonDefaults.buttonColors(containerColor = dismissButtonColor),
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                        Button(
+                            onClick = { onConfirm() },
+                            colors = ButtonDefaults.buttonColors(containerColor = confirmButtonColor),
                         ) {
-                        Text(text = "Tidak", color = Color.White)
+                            Text(text = "Iya", color = Color.White)
+                        }
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            onClick = { onDismiss() },
+                            colors = ButtonDefaults.buttonColors(containerColor = dismissButtonColor),
+                        ) {
+                            Text(text = "Tidak", color = Color.White)
+                        }
                     }
                 }
             }
