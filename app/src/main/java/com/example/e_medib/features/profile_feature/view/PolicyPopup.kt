31cee36b2dffafa7973eeefd070e8b2c9da6c919
ArmandFS <actuallyarmand@gmail.com>
2024-07-18 17:33:32 +0700

    modified privacy policy dialog popup in profile

--- app/src/main/java/com/example/e_medib/features/profile_feature/view/PolicyPopup.kt
+++ app/src/main/java/com/example/e_medib/features/profile_feature/view/PolicyPopup.kt
@@ -1,6 +1,7 @@
 package com.example.e_medib.features.profile_feature.view
 
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,8 +10,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,105 +32,120 @@ import androidx.compose.ui.unit.sp
 fun PolicyDialog(
     onDismiss: () -> Unit
 ) {
-    androidx.compose.material3.AlertDialog(
-        onDismissRequest = { onDismiss() },
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        modifier = Modifier
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+            .border(1.dp, Color.Black.copy(alpha = 0.6f), RoundedCornerShape(8.dp))
+    ) {
 
-        title = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 8.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
+        androidx.compose.material3.AlertDialog(
+            onDismissRequest = { onDismiss() },
+
+            title = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    contentAlignment = Alignment.Center
                 ) {
-                    Spacer(modifier = Modifier.width(8.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = "Persetujuan Keamanan Data",
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(start = 8.dp)
+                        )
+                    }
+                }
+            },
+            text = {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 6.dp, vertical = 8.dp)
+
+                ) {
                     Text(
-                        text = "Persetujuan Keamanan Data",
-                        fontSize = 18.sp,
+                        text = "Identitas Anda",
+                        style = MaterialTheme.typography.bodyMedium,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(start = 8.dp)
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = buildAnnotatedString {
+                            append("Identitas Anda, termasuk nama dan tanggal lahir, akan digunakan dalam aplikasi ini.")
+                        },
+                        textAlign = TextAlign.Justify,
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Divider(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                    )
+                    Text(
+                        text = "Data Kesehatan",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = buildAnnotatedString {
+                            append("Data kesehatan diabetes pengguna akan digunakan dalam aplikasi ini.")
+                        },
+                        textAlign = TextAlign.Justify,
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Divider(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+                    )
+                    Text(
+                        text = "Keamanan Data",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Start,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Text(
+                        text = buildAnnotatedString {
+                            append("Data pengguna akan aman dan terjamin selama pengguna bertanggung jawab atas keamanan kredensialnya sendiri.")
+                        },
+                        textAlign = TextAlign.Justify,
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onBackground
                     )
                 }
-            }
-        },
-        text = {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 6.dp, vertical = 8.dp)
-            ) {
-                Text(
-                    text = "Identitas Anda",
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Text(
-                    text = buildAnnotatedString {
-                        append("Identitas Anda, termasuk nama dan tanggal lahir, akan digunakan dalam aplikasi ini.")
-                    },
-                    textAlign = TextAlign.Justify,
-                    fontSize = 14.sp,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-                Divider(modifier = Modifier.padding(vertical = 8.dp), color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f))
-                Text(
-                    text = "Data Kesehatan",
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Text(
-                    text = buildAnnotatedString {
-                        append("Data kesehatan diabetes pengguna akan digunakan dalam aplikasi ini.")
-                    },
-                    textAlign = TextAlign.Justify,
-                    fontSize = 14.sp,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-                Divider(modifier = Modifier.padding(vertical = 8.dp), color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f))
-                Text(
-                    text = "Keamanan Data",
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start,
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Text(
-                    text = buildAnnotatedString {
-                        append("Data pengguna akan aman dan terjamin selama pengguna bertanggung jawab atas keamanan kredensialnya sendiri.")
-                    },
-                    textAlign = TextAlign.Justify,
-                    fontSize = 14.sp,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-            }
-        },
-        confirmButton = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = { onDismiss() },
-                    modifier = Modifier.padding(8.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF5799FC))
+            },
+            confirmButton = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Text(text = "Kembali")
+                    Button(
+                        onClick = { onDismiss() },
+                        modifier = Modifier.padding(8.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF5799FC))
+                    ) {
+                        Text(text = "Kembali")
+                    }
                 }
-            }
-        },
-        modifier = Modifier
-            .padding(horizontal = 2.dp, vertical = 6.dp)
-            .width(300.dp)
-    )
+            },
+            modifier = Modifier
+                .padding(horizontal = 2.dp, vertical = 6.dp)
+                .width(300.dp)
+        )
+    }
 }
